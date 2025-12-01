49cf5145ccb48da44643ca03d5e16a8e4a6b485a
Marco926-byte <malatesti.ma@gmail.com>
2025-12-01 12:49:27 +0100

    Add file Move2
    Stabile1

---
+++ Move.cpp
@@ -0,0 +1,56 @@
+#include "Move.h"
+Move::Move()
+{
+       
+}
+
+int Move::get_from_square() const
+{
+    return from_square;
+}
+
+void Move::set_from_square(int from)
+{
+    from_square=from;
+}
+
+int Move::get_to_square() const
+{
+    return to_square;
+}
+
+void Move::set_to_square(int to)
+{
+    to_square=to;
+}
+
+char Move::get_character_captured() const
+{
+    return character_captured;
+}
+
+void Move::set_character_captured(char new_char)
+{
+    character_captured=new_char;
+    
+}
+
+Piece* Move::get_piece_status() const
+{
+    return piece_status;
+}
+
+Piece* Move::get_piece_captured() const
+{
+    return piece_captured;
+}
+
+void Move::set_piece_status(Piece* p)
+{
+    piece_status=p;
+}
+
+void Move::set_piece_captured(Piece *pc)
+{
+    piece_captured=pc;
+}
---
+++ Move.h
@@ -0,0 +1,38 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+#include "Piece_factory/Piece.h"
+
+#include <wx/wx.h>
+
+class Move
+{
+private:
+    int from_square, to_square;
+
+    char character_captured;
+
+    Piece* piece_status=nullptr;
+    Piece* piece_captured=nullptr;
+public:
+    Move();
+
+    int get_from_square() const;
+    void set_from_square(int from);
+    int get_to_square() const;
+    void set_to_square(int to);
+    
+    char get_character_captured() const;
+    void set_character_captured(char new_char);
+
+    Piece* get_piece_status() const;
+    Piece* get_piece_captured() const;
+
+    void set_piece_status(Piece* p);
+    void set_piece_captured(Piece *pc);
+
+    
+};
+
+
+#endif //MOVE_H
